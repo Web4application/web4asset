42d58b7ffb359db6df7eaa731fb78a926f3aded5
Web4 <167559384+Web4application@users.noreply.github.com>
2025-07-17 03:17:22 +0200

    Add files via upload

---
+++ Next.js
@@ -0,0 +1,52 @@
+
+// context/Web3Modal.tsx
+
+'use client'
+
+import { createWeb3Modal, defaultConfig } from '@web3modal/ethers5/react'
+
+// Your WalletConnect Cloud project ID
+export const projectId = '558b268b1e26a373017cc04a3c40bbc5'
+
+// 2. Set chains
+const mainnet = {
+  chainId: 1,
+  name: 'Ethereum',
+  currency: 'ETH',
+  explorerUrl: 'https://etherscan.io',
+  rpcUrl: 'https://cloudflare-eth.com'
+}
+
+// 3. Create a metadata object
+const metadata = {
+  name: 'Kubucoin',
+  description: 'A Web3 application where crypto are been trade  with assurance ',
+  url: 'https://web3modal.com', // origin must match your domain & subdomain
+  icons: ['https://avatars.githubusercontent.com/u/37784886']
+}
+
+// 4. Create Ethers config
+const ethersConfig = defaultConfig({
+  /*Required*/
+  metadata,
+
+  /*Optional*/
+  enableEIP6963: true, // true by default
+  enableInjected: true, // true by default
+  enableCoinbase: true, // true by default
+  rpcUrl: '...', // used for the Coinbase SDK
+  defaultChainId: 1, // used for the Coinbase SDK
+})
+
+// 5. Create a Web3Modal instance
+createWeb3Modal({
+  ethersConfig,
+  chains: [mainnet],
+  projectId,
+  enableAnalytics: true, // Optional - defaults to your Cloud configuration
+  enableOnramp: true // Optional - false as default
+})
+
+export function Web3Modal({ children }) {
+  return children
+}
---
+++ Node.js
@@ -0,0 +1,4 @@
+npm i @web3modal/ethers ethers
+npm i @web3modal/wagmi wagmi viem @tanstack/react-query
+pnpm add @web3modal/solana
+npm i @walletconnect/web3wallet
